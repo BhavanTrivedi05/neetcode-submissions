2e5b7b0b21d536a7db4c0bdb2037c0d149bfa552
Bhavan Trivedi <bhavantrivedi05@gmail.com>
2026-09-02 16:47:19 -0400

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        for (int r = 0; r< grid.length;r++){
+            for (int c = 0; c< grid[0].length;c++){
+                if (grid[r][c] == '1'){
+                    count ++;
+                    sink(grid, r,c);
+                }
+            }
+        }
+        return count;
+    }
+
+    private void sink(char[][] grid, int r, int c){
+        if (r<0 || c<0 || r>= grid.length || c>= grid[0].length) return;
+        if (grid[r][c] != '1') return;
+
+        grid[r][c] = '0';
+        sink(grid,r+1,c);
+        sink(grid,r-1,c);
+        sink(grid,r,c+1);
+        sink(grid,r,c-1);
+    }
+}
